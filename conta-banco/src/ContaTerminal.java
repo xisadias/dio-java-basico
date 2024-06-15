bf6b14bf206c71564ed58d8e5c561ca5132090bf
Isadora <isadoradiasxavier@gmail.com>
2024-06-15 12:08:43 -0300

    feat: add dados bancários

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua conta: ");
+        int conta = scanner.nextInt();
+        
+        System.out.println("Digite sua agência: ");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome completo: ");
+        String nome = scanner.next();
+
+        System.out.println("Seu saldo: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Ola "+ nome + "!Obrigado por criar uma conta em nosso banco.");
+        System.out.println("Sua conta é " + conta + " agência " + agencia +  "e seu saldo R$" + saldo + " já está disponível para saque ");
+
+
+
+
+
     }
 }
